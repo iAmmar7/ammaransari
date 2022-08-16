4545c56e9cba21ceea36a2c0cb510f6e1420c9fc
Ammar Ansari <iammaransari@gmail.com>
2022-08-17 00:35:36 +0500

    navbar options update

--- components/Navbar/constants.js
+++ components/Navbar/constants.js
@@ -1,6 +1,8 @@
 export const pages = [
   { name: 'About', path: '/about' },
-  { name: 'Projects', path: '/projects' },
+  { name: 'Career', path: '/career' },
   { name: 'Skills', path: '/skills' },
+  { name: 'Projects', path: '/projects' },
   { name: 'Contact', path: '/contact' },
+  { name: 'Resume', path: '/resume' },
 ];
