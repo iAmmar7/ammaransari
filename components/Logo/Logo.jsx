30c81e0a3853dd3da98b9ed7298667ff2ad556a1
Ammar Ansari <iammaransari@gmail.com>
2022-08-17 00:01:38 +0500

    logo color and style modified

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 function Logo() {
   return (
     <Link href='/'>
-      <a className='btn-header font-bold text-2xl no-underline ml-3 hover:opacity-80 leading-10 -mt-1'>A</a>
+      <a className='btn-header font-bold text-2xl no-underline ml-3 hover:opacity-80 leading-10 -mt-1 hover:scale-150 hover:text-secondary hover:skew-y-6'>
+        A
+      </a>
     </Link>
   );
 }
