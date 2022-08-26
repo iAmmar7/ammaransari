af4c4cac587bf2398fc9401f263c28d3c69467d3
Ammar Ansari <iammaransari@gmail.com>
2022-08-26 23:19:15 +0500

    social links animation

--- components/SocialLinks/SocialLinks.jsx
+++ components/SocialLinks/SocialLinks.jsx
@@ -1,29 +1,44 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
 import { socialLinks } from '../../lib/socialMedia';
 import Icon from '../Icon/Icon';
 import Badge from '../Badge/Badge';
 
 function SocialLinks() {
+  const [hoveredLink, setHoveredLink] = useState(null);
+
   return (
     <div className='flex items-left gap-x-3 pb-2'>
       {socialLinks.map(({ title, url, icon }) => {
         return (
-          <div key={title} className='transition duration-md ease-base group relative'>
-            <a
+          <div key={title} className='relative'>
+            <motion.a
               href={url}
-              className='text-muted no-underline focus:text-primary cursor-pointer'
+              className='transition duration-md ease-base relative text-muted no-underline focus:text-primary cursor-pointer group'
               target='_blank'
               rel='noreferrer'
+              onHoverStart={() => setHoveredLink(title)}
+              onHoverEnd={() => setHoveredLink(null)}
             >
               <Icon
                 icon={icon}
                 className={`text-2xl text-primary group-hover:md:animate-pulse group-hover:text-secondary`}
               />
-            </a>
-            <span className='absolute top-9 right-1/2 translate-x-1/2 text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-md ease-base'>
-              <Badge type='secondary'>
-                <p>{title}</p>
-              </Badge>
-            </span>
+            </motion.a>
+            {hoveredLink === title && (
+              <motion.span
+                layoutId={title}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className='absolute top-9 right-1/2 translate-x-1/2 text-sm'
+              >
+                <Badge type='secondary'>
+                  <p>{title}</p>
+                </Badge>
+              </motion.span>
+            )}
           </div>
         );
       })}
