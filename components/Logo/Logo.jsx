aed54ea9165bd1defbc080f026bfb85ed9a77617
Ammar Ansari <iammaransari@gmail.com>
2022-08-27 15:05:37 +0500

    skew removed from the logo

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Logo() {
   return (
     <Link href='/'>
-      <a className='btn-header font-bold text-2xl no-underline ml-3 hover:opacity-80 leading-10 -mt-1 hover:scale-150 hover:text-secondary hover:skew-y-6 z-20'>
+      <a className='btn-header font-bold text-2xl no-underline ml-3 hover:opacity-80 leading-10 -mt-1 hover:scale-150 hover:text-secondary z-20'>
         A
       </a>
     </Link>
