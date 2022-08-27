494cb32ae104759a8f84d166e13642d4ff571169
Ammar Ansari <iammaransari@gmail.com>
2022-08-27 15:44:09 +0500

    inView bug fixes for mobile devices

--- components/Sections/AboutSection.jsx
+++ components/Sections/AboutSection.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import AboutMe from '../AboutMe/AboutMe';
 import Section from './Section';
+import { useBreakpoints } from '../../hooks';
 
 function AboutSection() {
+  const { sm } = useBreakpoints();
+
   return (
-    <Section id='about' navViewAmount={0.6}>
+    <Section id='about' navViewAmount={!sm ? 0.8 : 0.4}>
       <AboutMe />
     </Section>
   );
--- components/Sections/Section.jsx
+++ components/Sections/Section.jsx
@@ -1,24 +1,23 @@
-import { useRef } from 'react';
-import { motion, useInView } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import Navbar from '../Navbar/Navbar';
 
 function Section(props) {
   const { children, footer, id, navViewAmount = 1 } = props;
-  const ref = useRef(null);
-  const isInView = useInView(ref, { amount: navViewAmount });
+  const { ref, inView } = useInView({
+    threshold: navViewAmount,
+  });
 
   return (
-    <section
-      id={id}
-      ref={ref}
-      className='min-h-screen overflow-hidden flex flex-col justify-center relative snap-start py-28'
-    >
-      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: isInView ? 1 : 0 }}>
+    <section id={id} className='min-h-screen overflow-hidden flex flex-col justify-center relative snap-start py-32'>
+      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: inView ? 1 : 0 }} viewport={{ once: false }}>
         <Navbar />
       </motion.div>
       <div className='absolute inset-0 -z-10 bg-home-gradient opacity-40' />
-      <div className='my-0 mx-auto max-w-4xl py-0 px-5 relative'>{children}</div>
+      <div className='my-0 mx-auto max-w-4xl py-0 px-5 relative' ref={ref}>
+        {children}
+      </div>
       {footer}
     </section>
   );
