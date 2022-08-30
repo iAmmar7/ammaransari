0132810eb219a5d87091ddbd96b28c7025783679
Ammar Ansari <iammaransari@gmail.com>
2022-08-31 01:03:55 +0500

    description support for section component

--- components/Sections/Section.jsx
+++ components/Sections/Section.jsx
@@ -1,10 +1,11 @@
+import clsx from 'clsx';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 import Navbar from '../Navbar/Navbar';
 
 function Section(props) {
-  const { title, children, footer, id, navViewAmount = 1 } = props;
+  const { title, description, children, footer, id, navViewAmount = 1 } = props;
   const { ref, inView } = useInView({
     threshold: navViewAmount,
   });
@@ -20,10 +21,16 @@ function Section(props) {
       <div className='absolute inset-0 -z-10 bg-home-gradient opacity-40' />
       <div className='my-0 mx-auto max-w-4xl py-0 px-5 relative' ref={ref}>
         {title && (
-          <h1 className='text-2xl sm:text-4xl font-bold text-transparent tracking-wide bg-clip-text bg-gradient-to-r from-secondary to-tertiary mb-5 text-center sm:text-left'>
+          <h1
+            className={clsx(
+              'text-2xl sm:text-4xl font-bold text-transparent tracking-wide bg-clip-text bg-gradient-to-r from-secondary to-tertiary text-center sm:text-left',
+              description ? 'mb-0' : 'mb-5'
+            )}
+          >
             {title}
           </h1>
         )}
+        {description && <p className='text-muted text-sm text-center mb-5'>{description}</p>}
         {children}
       </div>
       <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: inView ? 1 : 0 }} viewport={{ once: false }}>
