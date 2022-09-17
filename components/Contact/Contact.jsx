d7d2dd0188d38e6e5d91fc387b607f831379767f
Ammar Ansari <iammaransari@gmail.com>
2022-09-18 02:13:26 +0500

    contact section with klotty

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,34 +1,70 @@
+import { useState } from 'react';
+
 import Button from '../Button/Button';
 import Icon from '../Icon/Icon';
+import Collapse from '../Collapse/Collapse';
 
 function Contact() {
-  const handleSendEmail = (e) => {
-    e.preventDefault();
+  const [sendFailed, setSendFailed] = useState(false);
+
+  const handleSendEmail = async (event) => {
+    event.preventDefault();
+    setSendFailed(false);
+
+    try {
+      const name = event.target.name.value;
+      const email = event.target.email.value;
+      const message = event.target.message.value;
+
+      const res = await fetch(`/api/email`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name,
+          email,
+          message,
+        }),
+      });
+
+      if (res.status !== 200) setSendFailed(true);
+    } catch (error) {
+      setSendFailed(true);
+    }
+  };
+
+  const handleResetError = () => {
+    if (sendFailed) setSendFailed(false);
   };
 
   return (
     <div>
-      <p className='text-muted'>
-        How can you connect with me? Well... I can think of three options; <br />
-        <span className='text-primary'>fill this form</span>, <br />
-        send me an email directly on my email address{' '}
-        <a href='mailto:iammar7@yahoo.com' target='_blank' rel='noreferrer' className='text-secondary'>
-          iammar7@yahoo.com
-        </a>
-        , <br />
-        or you may go through{' '}
-        <a
-          href='https://www.quora.com/How-can-I-train-my-pigeon-to-carry-messages-for-me'
-          target='_blank'
-          rel='noreferrer'
-          className='underline'
-        >
-          this
-        </a>{' '}
-        amazing question on quora. I&#39;ll try my best to respond in a timely manner
-      </p>
+      <div className='flex flex-col text-muted'>
+        <p>
+          How can you connect with me? Well... I can think of three options; <br />
+        </p>
+        <p className='text-primary'>fill this form,</p>
+        <p>
+          send me an email directly on my email address{' '}
+          <a href='mailto:iammar7@gmail.com' target='_blank' rel='noreferrer' className='text-secondary'>
+            iammar7@gmail.com
+          </a>
+          ,
+        </p>
+        <p>
+          or you may go through{' '}
+          <a
+            href='https://www.quora.com/How-can-I-train-my-pigeon-to-carry-messages-for-me'
+            target='_blank'
+            rel='noreferrer'
+            className='underline'
+          >
+            this
+          </a>{' '}
+          amazing question on quora. I&#39;ll try my best to respond in a timely manner
+        </p>
+      </div>
       <h2 className='mt-10 text-xl font-bold'>Send an email</h2>
-      <form className='flex flex-col max-w-md mt-4 gap-y-2' onSubmit={handleSendEmail}>
+      <form className='flex flex-col max-w-md mt-4 gap-y-2' onSubmit={handleSendEmail} onBlur={handleResetError}>
         {[
           { id: 'name', type: 'text', placeholder: 'John Doe', component: 'input' },
           { id: 'email', type: 'email', placeholder: 'john@doe.com', component: 'input' },
@@ -47,7 +83,15 @@ function Contact() {
             />
           </div>
         ))}
-        <div className='flex flex-col mb-3 gap-y-2'>
+        <Collapse show={sendFailed}>
+          <p className='text-sm text-red-400 mb-2'>
+            Unable to send email! Try sending it manually{' '}
+            <a href='mailto:iammar7@gmail.com' target='_blank' rel='noreferrer' className='text-secondary'>
+              iammar7@gmail.com
+            </a>
+          </p>
+        </Collapse>
+        <div className='flex flex-col gap-y-2'>
           <Button type='submit' endEnhancer={<Icon icon='ri-send-plane-fill' className='ml-2' />}>
             Send
           </Button>
---
+++ components/EmailTemplate/EmailTemplate.jsx
@@ -0,0 +1,19 @@
+export default function EmailTemplate(props) {
+  const { name, email, message } = props;
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <strong>Name:</strong> {name}
+        </li>
+        <li>
+          <strong>Email:</strong> {email}
+        </li>
+        <li>
+          <strong>Message:</strong> {message}
+        </li>
+      </ul>
+    </div>
+  );
+}
