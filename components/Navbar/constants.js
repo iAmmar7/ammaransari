cda7a294f538f13a4799cbfe37156d7c5bcbc0dc
Ammar Ansari <iammaransari@gmail.com>
2022-09-23 00:18:34 +0500

    navbar styles for current page

--- components/Navbar/constants.js
+++ components/Navbar/constants.js
@@ -1,7 +1,7 @@
 export const PAGES = [
-  { name: 'About', path: '/#about' },
-  { name: 'Experience', path: '/#experience' },
-  { name: 'Skills', path: '/#skills' },
-  { name: 'Projects', path: '/#projects' },
+  { name: 'About', path: '/about' },
+  { name: 'Experience', path: '/experience' },
+  { name: 'Skills', path: '/skills' },
+  { name: 'Projects', path: '/projects' },
   { name: 'Contact', path: '/#contact' },
 ];
