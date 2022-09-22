f9ccb13bce99120af92d9be2d663fb33ff71b8ff
Ammar Ansari <iammaransari@gmail.com>
2022-09-22 23:48:07 +0500

    external link color tailwind bug fixes

--- components/ExternalLink/ExternalLink.jsx
+++ components/ExternalLink/ExternalLink.jsx
@@ -2,6 +2,21 @@ import clsx from 'clsx';
 
 import Icon from '../Icon/Icon';
 
+// FIXME: Tailwind is not reading dynamic colors properly. Ideal: `from-${color} text-${color}`
+const colorMapper = {
+  primary: 'from-primary text-primary',
+  secondary: 'from-secondary text-secondary',
+  tertiary: 'from-tertiary text-tertiary',
+  muted: 'from-muted text-muted',
+  venturedive: 'from-venturedive text-venturedive',
+  careem: 'from-careem text-careem',
+  uber: 'from-uber text-uber',
+  carecloud: 'from-carecloud text-carecloud',
+  planz: 'from-planz text-planz',
+  sudofy: 'from-sudofy text-sudofy',
+  adres: 'from-adres text-adres',
+};
+
 function ExternalLink(props) {
   const { href, className, color = 'muted', showIcon = false, underline = false, children, ...otherProps } = props;
 
@@ -20,7 +35,7 @@ function ExternalLink(props) {
         <span
           className={clsx(
             'bg-left-bottom bg-gradient-to-r to-muted bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out',
-            `from-${color} text-${color}`
+            colorMapper[color]
           )}
         >
           {children}
