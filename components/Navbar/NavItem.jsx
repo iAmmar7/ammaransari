bcdc422aecc269b16e12d049c365b574a6fc16e2
Ammar Ansari <iammaransari@gmail.com>
2022-09-23 00:18:34 +0500

    navbar styles for current page

--- components/Navbar/NavItem.jsx
+++ components/Navbar/NavItem.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import clsx from 'clsx';
 
 function NavItem(props) {
   const { name, path } = props;
@@ -12,18 +11,16 @@ function NavItem(props) {
 
   useEffect(() => setHasMounted(true), []);
 
+  const css =
+    hasMounted && asPath.includes(path)
+      ? "text-secondary sm:text-muted cursor-pointer inline-block text-xs font-medium p-5 no-underline uppercase transition-colors duration-sm ease-base hover:text-secondary after:hidden sm:after:inline after:content-[''] after:absolute after:my-0 after:mx-auto after:inset-x-0 after:top-5 after:h-[2px] after:w-5 after:bg-text-secondary after:transition-opacity after:duration-sm after:ease-base after:opacity-100 hover:after:bg-text-primary"
+      : "text-muted cursor-pointer inline-block text-xs font-medium p-5 no-underline uppercase transition-colors duration-sm ease-base hover:text-secondary after:hidden md:after:inline after:content-[''] after:absolute after:my-0 after:mx-auto after:inset-x-0 after:top-5 after:h-[2px] after:w-5 after:bg-text-secondary after:transition-opacity after:duration-sm after:ease-base after:opacity-0 hover:after:bg-text-primary";
+
   return (
     <li>
       <Link href={path} passHref scroll={false}>
         <a className='border-0 relative hover:opacity-100 focus:opacity-100'>
-          <motion.span
-            className={clsx(
-              "text-muted cursor-pointer inline-block text-xs font-medium p-5 no-underline uppercase transition-colors duration-sm ease-base hover:text-secondary after:hidden md:after:inline after:content-[''] after:absolute after:my-0 after:mx-auto after:inset-x-0 after:top-5 after:h-[2px] after:w-5 after:bg-text-secondary after:transition-opacity after:duration-sm after:ease-base after:opacity-0 hover:after:bg-text-primary",
-              hasMounted && asPath === path && 'after:opacity-100 text-secondary md:text-muted'
-            )}
-            onHoverStart={() => setIsHovered(true)}
-            onHoverEnd={() => setIsHovered(false)}
-          >
+          <motion.span className={css} onHoverStart={() => setIsHovered(true)} onHoverEnd={() => setIsHovered(false)}>
             {isHovered && (
               <motion.span
                 className='absolute top-[-13px] inset-x-0 bg-secondary p-5 rounded-base -z-[1]'
