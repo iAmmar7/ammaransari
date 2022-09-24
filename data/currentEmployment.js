66760342523ca80ed09269c3c58f275dd373f41d
Ammar Ansari <iammaransari@gmail.com>
2022-09-24 16:18:51 +0500

    current employment section

---
+++ data/currentEmployment.js
@@ -0,0 +1,31 @@
+const currentEmployement = {
+  id: 1,
+  title: 'Software Engineer',
+  company: 'VentureDive',
+  companyTag: 'venturedive',
+  companyUrl: 'https://venturedive.com',
+  companyTheme: '#EE2938',
+  type: 'Full-time',
+  startDate: 'Jul 2021',
+  location: 'Karachi, Pakistan, Remote',
+  summary: [
+    'Responsible for both frontend and server-side development.',
+    {
+      clientId: 2,
+      client: 'Careem',
+      clientTag: 'careem',
+      clientUrl: 'https://www.careem.com',
+      clientTheme: '#47A23F',
+      clientSubsidiary: 'Uber',
+      clientSubsidiaryTag: 'uber',
+      clientSubsidiaryUrl: 'https://www.uber.com',
+      clientSubsidiaryTheme: '#000000',
+      clientSummary: [
+        'Member of the CareemPay team.',
+        'Build and maintain the whole flow of “Pay with Careem” with other team members.',
+      ],
+    },
+  ],
+};
+
+export default currentEmployement;
