19a1701207d1f5f0a976372cd7922a856674fdad
Ammar Ansari <iammaransari@gmail.com>
2022-09-25 21:52:57 +0500

    current tech-stack section UI fix

--- components/Sections/CurrentTechStack.jsx
+++ components/Sections/CurrentTechStack.jsx
@@ -23,46 +23,44 @@ const skillToSVGMapper = (skill) => {
 
 function SkillSection() {
   return (
-    <Section id='skills' title='Current Tech-Stack' next='projects'>
-      <div className=''>
-        <ul className='grid grid-cols-3 sm:grid-cols-4 gap-4 px-2 md:px-0'>
-          {CURRENT_TECH_STACK.map((tech) => {
-            const SVG = skillToSVGMapper(tech.name);
-            return (
-              <Link
-                key={tech.name}
-                href={{ pathname: '/projects', query: { skill: tech.name === 'TypeScript' ? null : tech.name } }}
-                passHref
-              >
-                <a className='group relative bg-muted cursor-pointer rounded-base shadow-md backdrop-blur-md transition-all ease-base duration-md hover:translate-x-0 hover:-translate-y-1.5 hover:rotate-0 hover:skew-x-0 hover:skew-y-0 hover:scale-x-100 hover:scale-y-100'>
-                  <SVG.Component
-                    className={clsx(
-                      'flex items-center justify-center sm:max-w-[112px] sm:h-28 max-w-[86px] h-20 rounded-base aagn-middle font-semibold p-2 sm:p-4 transition duration-md ease-base group-hover:opacity-10',
-                      SVG.color
-                    )}
-                    height='100%'
-                    width='100%'
-                  />
-                  <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full flex justify-center transition duration-md ease-base opacity-0 group-hover:opacity-100 font-bold text-sm sm:text-lg'>
-                    {tech.name}
-                  </p>
-                </a>
-              </Link>
-            );
-          })}
-        </ul>
-        <Link href='/skills' passHref>
-          <Button
-            as='a'
-            type='default'
-            size='sm'
-            className='mt-4'
-            endEnhancer={<Icon icon='ri-arrow-right-line' className='ml-1' />}
-          >
-            Other skills
-          </Button>
-        </Link>
-      </div>
+    <Section id='skills' title='Current tech-stack' next='projects'>
+      <ul className='grid grid-cols-3 sm:grid-cols-4 gap-4 px-2 md:px-0'>
+        {CURRENT_TECH_STACK.map((tech) => {
+          const SVG = skillToSVGMapper(tech.name);
+          return (
+            <Link
+              key={tech.name}
+              href={{ pathname: '/projects', query: { skill: tech.name === 'TypeScript' ? null : tech.name } }}
+              passHref
+            >
+              <a className='group relative bg-muted cursor-pointer rounded-base shadow-md backdrop-blur-md transition-all ease-base duration-md hover:translate-x-0 hover:-translate-y-1.5 hover:rotate-0 hover:skew-x-0 hover:skew-y-0 hover:scale-x-100 hover:scale-y-100'>
+                <SVG.Component
+                  className={clsx(
+                    'flex items-center justify-center sm:max-w-[112px] sm:h-28 max-w-[86px] h-20 rounded-base aagn-middle font-semibold p-2 sm:p-4 transition duration-md ease-base group-hover:opacity-10',
+                    SVG.color
+                  )}
+                  height='100%'
+                  width='100%'
+                />
+                <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full flex justify-center transition duration-md ease-base opacity-0 group-hover:opacity-100 font-bold text-sm sm:text-lg'>
+                  {tech.name}
+                </p>
+              </a>
+            </Link>
+          );
+        })}
+      </ul>
+      <Link href='/skills' passHref>
+        <Button
+          as='a'
+          type='default'
+          size='sm'
+          className='mt-4'
+          endEnhancer={<Icon icon='ri-arrow-right-line' className='ml-1' />}
+        >
+          Other skills
+        </Button>
+      </Link>
     </Section>
   );
 }
