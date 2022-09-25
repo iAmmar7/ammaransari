b7c7772e3806b17b32d7d13679c4f8f80770072f
Ammar Ansari <iammaransari@gmail.com>
2022-09-25 20:27:15 +0500

    horizontal padding for mobile screen

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 function Logo() {
   return (
     <Link href='/'>
-      <a className='btn-header font-bold text-2xl no-underline ml-3 leading-10 -mt-1 hover:text-secondary z-20'>A.</a>
+      <a className='btn-header font-bold text-2xl no-underline ml-1 sm:ml-3 leading-10 -mt-1 hover:text-secondary z-20'>
+        A.
+      </a>
     </Link>
   );
 }
