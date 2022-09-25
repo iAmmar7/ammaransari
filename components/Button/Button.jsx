4d279714ae758747e3855ec6529d2eb30fbfdbda
Ammar Ansari <iammaransari@gmail.com>
2022-09-25 20:32:27 +0500

    button bg color bug fixes

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -18,10 +18,11 @@ const Button = forwardRef((props, ref) => {
   const Component = as ?? 'button';
 
   const colors = useMemo(() => {
-    if (type === 'default') return 'bg-transparent text-muted hover:text-secondary after:bg-secondary';
-    if (type === 'secondary') return 'bg-secondary text-secondary hover:text-black';
-    if (type === 'tertiary') return 'bg-muted text-muted hover:text-black';
-    return 'bg-text-secondary text-black';
+    if (type === 'default')
+      return 'bg-transparent text-muted hover:text-secondary hover:bg-secondary after:bg-secondary';
+    if (type === 'secondary') return 'bg-secondary text-secondary hover:text-black after:bg-white';
+    if (type === 'tertiary') return 'bg-muted text-muted hover:text-black after:bg-white';
+    return 'bg-text-secondary text-black after:bg-white';
   }, [type]);
 
   const padding = useMemo(() => {
@@ -33,7 +34,7 @@ const Button = forwardRef((props, ref) => {
     <Component
       ref={ref}
       className={clsx(
-        "relative inline-block h-auto min-h-0 w-auto cursor-pointer overflow-hidden opacity-100 text-center align-middle text-sm font-medium z-0 transition-all ease-base duration-500 after:content-[''] after:absolute after:left-0 after:top-0 after:inline-block after:h-full after:w-full after:origin-bottom after:scale-y-0 after:transform after:opacity-100 after:bg-white after:transition-transform after:ease-base after:duration-md hover:after:origin-top hover:after:scale-y-100 hover:after:transform active:transition-all active:ease-base active:duration-md",
+        "relative inline-block h-auto min-h-0 w-auto cursor-pointer overflow-hidden opacity-100 text-center align-middle text-sm font-medium z-0 transition-all ease-base duration-500 after:content-[''] after:absolute after:left-0 after:top-0 after:inline-block after:h-full after:w-full after:origin-bottom after:scale-y-0 after:transform after:opacity-100 after:transition-transform after:ease-base after:duration-md hover:after:origin-top hover:after:scale-y-100 hover:after:transform active:transition-all active:ease-base active:duration-md",
         colors,
         padding,
         rounded ? 'rounded-full' : 'rounded-base',
