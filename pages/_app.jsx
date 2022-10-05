77498c5a81c8108a9e71f9883e81c882eba3b643
Ammar Ansari <iammaransari@gmail.com>
2022-10-05 23:36:45 +0500

    google analytics bug fixes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,14 +11,14 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Script
         strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
       />
       <Script id='google-analytics' strategy='afterInteractive'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
             page_path: window.location.pathname,
           });
        `}
