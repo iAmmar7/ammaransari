712fde6451b06625333a793b7531c0867acc0cea
Ammar Ansari <iammaransari@gmail.com>
2022-10-07 01:50:41 +0500

    crossorigin warning bug fixes

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <meta content='summary_large_image' name='twitter:card' />
           <meta name='theme-color' content='#08070b' />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
           <link href='https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap' rel='stylesheet' />
         </Head>
         <body>
