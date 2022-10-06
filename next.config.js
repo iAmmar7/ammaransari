ffe316f0428c51ffc63e0b4d0c23cb6d5096626e
Ammar Ansari <iammaransari@gmail.com>
2022-10-07 00:34:05 +0500

    project and experience page size and load time reduced

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -20,4 +16,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
