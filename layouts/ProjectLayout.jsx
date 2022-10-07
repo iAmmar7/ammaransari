352838adfc2f4a7672d76986d2ab237dfbf8d355
Ammar Ansari <iammaransari@gmail.com>
2022-10-08 00:17:09 +0500

    project card semantic update

--- layouts/ProjectLayout.jsx
+++ layouts/ProjectLayout.jsx
@@ -49,82 +49,86 @@ function Project(props) {
             </div>
 
             {/* Thumbnail */}
-            <div className='bg-muted opacity-100 rounded-base shadow-md backdrop-blur-md transition-all ease-base duration-md p-0 group hover:translate-x-0 hover:-translate-y-1.5 hover:rotate-0 hover:skew-x-0 hover:skew-y-0 hover:scale-x-100 hover:scale-y-100'>
-              <div className='relative overflow-hidden rounded-base'>
-                <div className='blur-none transition-all duration-md group-hover:blur'>
-                  <Image
-                    alt={name}
-                    src={!isEmpty(thumbnail) ? thumbnail : 'https://via.placeholder.com/200.png'}
-                    quality={100}
-                    placeholder='blur'
-                    blurDataURL={blurryDataProfile}
-                    width={'100%'}
-                    height={xs || sm ? 40 : 30}
-                    objectFit='cover'
-                    layout='responsive'
-                  />
+            <figure>
+              <div className='bg-muted opacity-100 rounded-base shadow-md backdrop-blur-md transition-all ease-base duration-md p-0 group hover:translate-x-0 hover:-translate-y-1.5 hover:rotate-0 hover:skew-x-0 hover:skew-y-0 hover:scale-x-100 hover:scale-y-100'>
+                <div className='relative overflow-hidden rounded-base'>
+                  <picture className='blur-none transition-all duration-md group-hover:blur'>
+                    <Image
+                      alt={name}
+                      src={!isEmpty(thumbnail) ? thumbnail : 'https://via.placeholder.com/200.png'}
+                      quality={100}
+                      placeholder='blur'
+                      blurDataURL={blurryDataProfile}
+                      width={'100%'}
+                      height={xs || sm ? 40 : 30}
+                      objectFit='cover'
+                      layout='responsive'
+                    />
+                  </picture>
+                  <div className='absolute left-0 top-0 z-10 flex h-full w-full -translate-x-full transform items-center justify-center gap-4 overflow-hidden bg-muted/80 transition-all duration-md group-hover:translate-x-0'>
+                    {code && (
+                      <a
+                        className='inline-flex h-10 min-h-0 w-10 items-center justify-center rounded-full bg-primary p-0'
+                        href={code}
+                        target='blank'
+                      >
+                        <Icon icon='ri-code-s-slash-line' className='text-xl' />
+                      </a>
+                    )}
+                    {url && (
+                      <a
+                        className='inline-flex h-10 min-h-0 w-10 items-center justify-center rounded-full bg-primary p-0'
+                        href={url}
+                        target='blank'
+                      >
+                        <Icon icon='ri-external-link-line' className='text-xl' />
+                      </a>
+                    )}
+                  </div>
                 </div>
-                <div className='absolute left-0 top-0 z-10 flex h-full w-full -translate-x-full transform items-center justify-center gap-4 overflow-hidden bg-muted/80 transition-all duration-md group-hover:translate-x-0'>
+              </div>
+
+              {/* Description */}
+              <figcaption>
+                <div className='w-full mt-4'>
+                  <Badge className='text-xs md:text-sm'>{capitalize(domain)}</Badge>
+                  <WavyText
+                    as='h1'
+                    className='text-3xl sm:text-5xl font-display font-bold leading-[1.1] mt-4 sm:mt-2 mb-2 sm:mb-5 text-tertiary'
+                    text={name}
+                    delay={0.6}
+                  />
+                  <p className='text-sm lg:text-base leading-relaxed max-w-prose mb-12'>{summary}</p>
+                  <div className='flex gap-x-2 border-b-[0.5px] border-muted pb-3'>
+                    {take(technologies, 3).map((tech) => (
+                      <Badge className='text-xs md:text-sm' key={tech}>
+                        {tech}
+                      </Badge>
+                    ))}
+                  </div>
+                </div>
+
+                {/* External links */}
+                <div className='absolute flex items-center space-x-4 bottom-3 right-4 md:right-0'>
                   {code && (
-                    <a
-                      className='inline-flex h-10 min-h-0 w-10 items-center justify-center rounded-full bg-primary p-0'
-                      href={code}
-                      target='blank'
-                    >
-                      <Icon icon='ri-code-s-slash-line' className='text-xl' />
-                    </a>
+                    <ExternalLink title='View source code'>
+                      <Icon
+                        icon='ri-code-s-slash-fill'
+                        className='text-base md:text-xl hover:text-tertiary transition-colors duration-sm ease-base'
+                      />
+                    </ExternalLink>
                   )}
                   {url && (
-                    <a
-                      className='inline-flex h-10 min-h-0 w-10 items-center justify-center rounded-full bg-primary p-0'
-                      href={url}
-                      target='blank'
-                    >
-                      <Icon icon='ri-external-link-line' className='text-xl' />
-                    </a>
+                    <ExternalLink title='View live application'>
+                      <Icon
+                        icon='ri-external-link-line'
+                        className='text-base md:text-lg hover:text-tertiary transition-colors duration-sm ease-base'
+                      />
+                    </ExternalLink>
                   )}
                 </div>
-              </div>
-            </div>
-
-            {/* Description */}
-            <div className='w-full mt-4'>
-              <Badge className='text-xs md:text-sm'>{capitalize(domain)}</Badge>
-              <WavyText
-                as='h1'
-                className='text-3xl sm:text-5xl font-display font-bold leading-[1.1] mt-4 sm:mt-2 mb-2 sm:mb-5 text-tertiary'
-                text={name}
-                delay={0.6}
-              />
-              <p className='text-sm lg:text-base leading-relaxed max-w-prose mb-12'>{summary}</p>
-              <div className='flex gap-x-2 border-b-[0.5px] border-muted pb-3'>
-                {take(technologies, 3).map((tech) => (
-                  <Badge className='text-xs md:text-sm' key={tech}>
-                    {tech}
-                  </Badge>
-                ))}
-              </div>
-            </div>
-
-            {/* External links */}
-            <div className='absolute flex items-center space-x-4 bottom-3 right-4 md:right-0'>
-              {code && (
-                <ExternalLink title='View source code'>
-                  <Icon
-                    icon='ri-code-s-slash-fill'
-                    className='text-base md:text-xl hover:text-tertiary transition-colors duration-sm ease-base'
-                  />
-                </ExternalLink>
-              )}
-              {url && (
-                <ExternalLink title='View live application'>
-                  <Icon
-                    icon='ri-external-link-line'
-                    className='text-base md:text-lg hover:text-tertiary transition-colors duration-sm ease-base'
-                  />
-                </ExternalLink>
-              )}
-            </div>
+              </figcaption>
+            </figure>
           </div>
         </motion.section>
 
