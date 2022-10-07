7af38efc8372c4d4b34697deac7aaa55c40a4b77
Ammar Ansari <iammaransari@gmail.com>
2022-10-08 00:00:31 +0500

    lottie react removed for performance

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,4 +1,4 @@
-import Lottie from 'lottie-react';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { Fragment } from 'react';
 
@@ -6,7 +6,38 @@ import Meta from '../components/Meta';
 import Button from '../components/Button';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import icon404 from '../public/icons/404.json';
+import clsx from 'clsx';
+
+const container = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.05, delayChildren: i * 0.6 },
+  }),
+};
+
+const child = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      damping: 12,
+      stiffness: 200,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: 20,
+    transition: {
+      type: 'spring',
+      damping: 12,
+      stiffness: 200,
+    },
+  },
+};
 
 function Custom404(props) {
   return (
@@ -18,13 +49,40 @@ function Custom404(props) {
         <Navbar />
         <main className='px-0 pt-28 pb-12 sm:pt-20 sm:pb-14 overflow-hidden flex-auto grid'>
           <div className='flex flex-col gap-y-2 items-center justify-center px-4'>
-            <Lottie className='h-auto' animationData={icon404} />
-            <p className='text-muted md:text-lg mb-6 tracking-wider text-center'>Whoops! It seems like you are lost.</p>
-            <Link href='/' passHref>
-              <Button as='a' title='View home page'>
-                Go to home
-              </Button>
-            </Link>
+            <motion.h1
+              variants={container}
+              initial='hidden'
+              animate='visible'
+              className='flex overflow-hidden flex-wrap text-3xl sm:text-9xl font-display font-bold leading-[1.1] mt-4 sm:mt-2 mb-2 sm:mb-5'
+            >
+              {[4, 0, 4].map((letter, index) => {
+                return (
+                  <motion.span
+                    key={index}
+                    variants={child}
+                    className={clsx(
+                      'relative',
+                      letter ? 'text-secondary' : 'text-primary -top-3 animate-pulse transform rotate-12'
+                    )}
+                  >
+                    {letter}
+                  </motion.span>
+                );
+              })}
+            </motion.h1>
+            <p className='text-muted md:text-lg mb-6 text-center'>Whoops! It seems like you are lost.</p>
+            <div className='flex gap-2'>
+              <Link href='/' passHref>
+                <Button as='a' title='View home page'>
+                  Go to home
+                </Button>
+              </Link>
+              <Link href='/#contact' passHref>
+                <Button as='a' title='Contact Ammar Ansari' type='secondary' className='border border-secondary'>
+                  Contact me
+                </Button>
+              </Link>
+            </div>
           </div>
         </main>
         <Footer />
