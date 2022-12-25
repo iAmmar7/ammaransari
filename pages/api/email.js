663cfbc5a8857efd0cdc0366a631bfc2e0168329
Ammar Ansari <iammaransari@gmail.com>
2022-12-25 22:12:57 +0500

    nodemailer setup

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,22 +1,52 @@
-import { Klotty } from 'klotty';
-
-const klotty = new Klotty(process.env.KLOTTY_API_KEY);
+import nodemailer from 'nodemailer';
 
 import EmailTemplate from '../../components/EmailTemplate/EmailTemplate';
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
 
 export default async function sendEmail(req, res) {
   try {
     const data = req.body;
 
-    await klotty.sendEmail({
-      from: 'web@ammaransari.com',
-      to: 'iammar7@yahoo.com',
+    await transporter.sendMail({
+      from: 'maansaridot@gmail.com',
+      to: 'iammaransari@gmail.com',
       subject: `${data.name} - via ammaransari.com`,
-      react: <EmailTemplate {...data} />,
+      html: <EmailTemplate {...data} />,
     });
 
     res.status(200).json({ message: 'Email sent' });
   } catch (e) {
+    console.log(e);
     res.status(500).json({ message: e.message });
   }
 }
+
+// import { Klotty } from 'klotty';
+
+// const klotty = new Klotty(process.env.KLOTTY_API_KEY);
+
+// export default async function sendEmail(req, res) {
+//   try {
+//     const data = req.body;
+
+//     await klotty.sendEmail({
+//       from: 'web@ammaransari.com',
+//       to: 'iammar7@yahoo.com',
+//       subject: `${data.name} - via ammaransari.com`,
+//       react: <EmailTemplate {...data} />,
+//     });
+
+//     res.status(200).json({ message: 'Email sent' });
+//   } catch (e) {
+//     res.status(500).json({ message: e.message });
+//   }
+// }
