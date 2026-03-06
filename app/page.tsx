2dde361ada7770c962134c6125d804da9770cc2a
Ammar Ansari <iammaransari@gmail.com>
2026-03-06 23:51:18 +0500

    feat: add site name constant and update home page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { SITE_NAME } from '@/lib/constants';
+
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center'>
-      <h1 className='text-4xl font-bold'>ammaransari.dev</h1>
+      <h1 className='text-4xl font-bold'>{SITE_NAME}</h1>
     </div>
   );
 }
